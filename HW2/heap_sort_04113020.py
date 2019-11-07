12147fcc23f566cec58e7a0884348f189ca7785b
Wen Sui (Ellier) <okpersist@gmail.com>
2019-11-07 20:32:33 +0800

    Update heap_sort_04113020.py

--- HW2/heap_sort_04113020.py
+++ HW2/heap_sort_04113020.py
@@ -1,8 +1,43 @@
 # 原創程式碼
+nums = [1, 4, 2, 3.6, -1, 0, 25, -34, 8, 9, 1, 0]
+result = []
+size = len(nums)
 
 class Solution(object):
-    def heap_sort(self, nums):
-        """
-        :type nums: List[int] ex:[3,2,-4,6,4,2,19],[5,1,1,2,0,0]
-        :rtype: List[int] ex:[-4,2,2,3,4,6,19],[0,0,1,1,2,5]
-        """
+    
+    def swap(self, nums, a, b):
+        nums[a], nums[b] = nums[b], nums[a]
+    
+    def heapify(self, nums, index):
+        left = 2*index + 1
+        right = 2*index +2
+        size = len(nums)
+        
+        if size<2:
+            return nums
+        
+        if left <= size-1 and nums[left] > nums[index]:
+            self.swap(nums, index, left)
+            self.heapify(nums, left)
+        if right <= size-1 and nums[right] > nums[index]:
+            self.swap(nums, index, right)
+            self.heapify(nums, right)
+    
+    #[4,3.6,2,1] -> [1,3.6,2,4]
+    def heapify_down(self, nums):
+        for rounds in range(size-1, -1, -1):
+            self.swap(nums, 0, rounds)
+            result.append(nums.pop())
+            self.heapify(nums, 0)
+        return result
+        
+    def heap_sort(self,nums):
+        for element in range(size-1, -1, -1):
+            self.heapify(nums, element)
+        
+        ans = self.heapify_down(nums)
+        ans = ans[::-1]
+        return ans
+
+test = Solution()
+test.heap_sort(nums)
