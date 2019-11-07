1de2b38c2115b8ba58ec34e91057ca55bc54de08
Wen Sui (Ellier) <okpersist@gmail.com>
2019-11-07 22:39:58 +0800

    Update merge_sort_04113020.py

--- HW2/merge_sort_04113020.py
+++ HW2/merge_sort_04113020.py
@@ -1,8 +1,37 @@
 # 原創程式碼
 
+nums = [1, 4, 2, 3.6]
 class Solution(object):
+    def divide(self, nums):    
+        if len(nums)<2:
+            return nums
+        else:
+            mid = len(nums)//2
+            left = nums[:mid]
+            right = nums[mid:]
+
+            left = self.divide(left)
+            right = self.divide(right)
+            merge = self.merge(left,right)
+        return merge
+            
+    def merge(self, left, right):
+        sorted_result = []
+        while left and right:
+            if left[0] < right[0]:
+                sorted_result.append(left.pop(0))
+            else:
+                sorted_result.append(right.pop(0))
+
+        if left:
+            sorted_result += left
+        else:
+            sorted_result += right
+        return sorted_result
+    
     def merge_sort(self, nums):
-        """
-        :type nums: List[int] ex:[3,2,-4,6,4,2,19],[5,1,1,2,0,0]
-        :rtype: List[int] ex:[-4,2,2,3,4,6,19],[0,0,1,1,2,5]
-        """
+        ans = self.divide(nums)
+        print(ans)
+        
+A = Solution()
+A.merge_sort(nums)
