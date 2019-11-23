0d15ed7b68b0127130f7d20d9a8b176a95e9777d
Wen Sui (Ellier) <okpersist@gmail.com>
2019-11-23 10:48:32 +0800

    Create hash_table_04113020.py

---
+++ HW4/hash_table_04113020.py
@@ -0,0 +1,37 @@
+```
+author: 汶穗
+period: 19.11.23 - 19.12.06
+```
+
+class ListNode:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+        """
+        :type val: int
+        :type next: ListNode
+        :rtype: None        
+        """
+class MyHashSet:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.data = [None] * capacity
+        """
+        :type capacity: int
+        :rtype: None
+        """
+    def add(self, key):
+        """
+        :type key: int
+        :rtype: None
+        """
+    def remove(self, key):
+        """
+        :type key: int
+        :rtype: None
+        """
+    def contains(self, key):
+        """
+        :type key: int
+        :rtype: bool(True or False)
+        """
